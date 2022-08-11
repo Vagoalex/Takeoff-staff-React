3a96b0066f9f966077c3aeb1e3bf7e6a0eeb90d4
Vago Aleksandr <vagoalex666@mail.ru>
2022-08-12 01:08:57 +0300

    feat: completed form validation

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,4 +1,8 @@
 import { FC, useState } from 'react';
+import { useFormik, FormikProps } from 'formik';
+import formValidate from 'helpers/formValidate';
+import { IUser } from 'types/IUser';
+import './Form.scss';
 
 interface FormProps {
   title: string;
@@ -9,21 +13,85 @@ const Form: FC<FormProps> = ({ title, handleClick }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const formik: FormikProps<IUser> = useFormik<IUser>({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+
+    validate: (values: IUser) => formValidate(values),
+    onSubmit: (values: IUser, { resetForm }) => {
+      resetForm();
+      handleClick(values.username, values.password);
+    },
+  });
+
   return (
-    <div>
-      <input
-        type='text'
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder='Имя пользователя'
-      />
-      <input
-        type='password'
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder='Пароль'
-      />
-      <button onClick={() => handleClick(username, password)}>{title}</button>
+    <div className='Form-container'>
+      <form className='Form' onSubmit={formik.handleSubmit}>
+        <div className='Form-elements'>
+          <div className='Form-elements__field'>
+            <label
+              className='Form-elements__label label-username'
+              htmlFor='username'
+            >
+              username
+            </label>
+            <input
+              className={`Form-elements__input ${
+                formik.touched.username && formik.errors.username
+                  ? 'form-input-required'
+                  : ''
+              }`}
+              id='username'
+              name='username'
+              type='text'
+              value={formik.values.username}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.username && formik.errors.username ? (
+              <div className='form-error-msg'>{formik.errors.username}</div>
+            ) : null}
+          </div>
+
+          <div className='Form-elements__field'>
+            <label
+              className='Form-elements__label label-password'
+              htmlFor='password'
+            >
+              password
+            </label>
+            <input
+              className={`Form-elements__input ${
+                formik.touched.password && formik.errors.password
+                  ? 'form-input-required'
+                  : ''
+              }`}
+              id='password'
+              name='password'
+              type='password'
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.password && formik.errors.password ? (
+              <div className='form-error-msg'>{formik.errors.password}</div>
+            ) : null}
+          </div>
+        </div>
+
+        <button
+          className={`Form__btn ${
+            formik.touched.username && formik.errors.password
+              ? 'form-button-disabled'
+              : ''
+          }`}
+          type='submit'
+        >
+          {title}
+        </button>
+      </form>
     </div>
   );
 };
